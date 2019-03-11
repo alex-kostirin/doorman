2dce2255a65306cbeea4830bb933acb8ca803cd7
a.kostyrin <a.kostyrin@corp.vk.com>
2019-03-11 18:17:28 +0300

    remove severity

--- doorman/plugins/logs/wsgi.py
+++ doorman/plugins/logs/wsgi.py
@@ -10,7 +10,6 @@ from doorman.utils import extract_results
 
 class LogWSGIPlugin(AbstractLogsPlugin):
     def __init__(self, config):
-        self.minimum_severity = config.get('DOORMAN_MINIMUM_OSQUERY_LOG_LEVEL')
         self.fp = wsgi_errors_stream
 
     @property
@@ -21,16 +20,11 @@ class LogWSGIPlugin(AbstractLogsPlugin):
         if self.fp is None:
             return
 
-        fp = self.fp
-        minimum_severity = self.minimum_severity
-
         host_identifier = kwargs.get('host_identifier')
         created = dt.datetime.utcnow().isoformat()
 
         try:
             for item in data.get('data', []):
-                if int(item['severity']) < minimum_severity:
-                    continue
 
                 if 'created' in item:
                     item['created'] = item['created'].isoformat()
@@ -47,16 +41,14 @@ class LogWSGIPlugin(AbstractLogsPlugin):
                     'filename': item.get('filename', ''),
                     'osquery_version': item.get('version'),  # be null
                     'created': created,
-                }, fp)
-                fp.write('\r\n')
+                }, self.fp)
+                self.fp.write('\r\n')
         finally:
-            fp.flush()
+            self.fp.flush()
 
     def handle_result(self, data, **kwargs):
         if self.fp is None:
             return
-
-        fp = self.fp
 
         host_identifier = kwargs.get('host_identifier')
         created = dt.datetime.utcnow().isoformat()
@@ -72,7 +64,7 @@ class LogWSGIPlugin(AbstractLogsPlugin):
                     'columns': item.columns,
                     'name': item.name,
                     'created': created,
-                }, fp)
-                fp.write('\r\n')
+                }, self.fp)
+                self.fp.write('\r\n')
         finally:
-            fp.flush()
+            self.fp.flush()
